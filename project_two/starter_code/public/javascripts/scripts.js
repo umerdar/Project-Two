3f90e49cc776c4475c455d0a293c8a2b082829fa
Loaner One <nycdeployment@Ians-Air.home>
2015-01-25 20:30:11 -0500

    Updates

---
+++ project_two/starter_code/public/javascripts/scripts.js
@@ -0,0 +1,35 @@
+console.log("connected")
+
+// var button1 = document.querySelector("#all-categories");
+// var button2 = document.querySelector("#all-contacts");
+
+
+// function returnallCategories() {
+
+//     $.ajax({
+//         url: '/categories',
+//         method: 'GET',
+//         dataType: 'json'
+//     }).done(function(data){
+//       console.log(data);
+
+//   });
+//  }
+
+// button1.addEventListener("click", function(){
+
+//   returnallCategories();
+
+// });
+
+// function returnallCategories() {
+
+//     $.ajax({
+//         url: '/categories',
+//         method: 'GET',
+//         dataType: 'json'
+//     }).done(function(data){
+//       console.log(data)
+
+//   })
+//  }
