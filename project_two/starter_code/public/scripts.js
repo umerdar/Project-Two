ac7892eebaec8a1877cd2a5d52549bba24db0363
Loaner One <nycdeployment@Ians-Air.home>
2015-01-27 17:52:10 -0500

    actual progress

--- project_two/starter_code/public/scripts.js
+++ project_two/starter_code/public/scripts.js
@@ -2,8 +2,8 @@ console.log("connected")
 
 var button1 = document.querySelector("#all-categories");
 var button2 = document.querySelector("#all-contacts");
-var button3 = document.querySelector("#create-contact");
-
+var button3 = document.querySelector("#add-contact");
+var button4 = document.querySelector("#delete-contact");
 
 function returnallContacts() {
 
@@ -40,29 +40,56 @@ function postEntry(info) {
         dataType: 'json',
         data: JSON.stringify(info)
     }).done(function(data){
+        console.log(data);
+        debugger
 //postentry posts to database, gets response
 //now make a function that takes response and
 //appends it to the DOM. Call this function right here
 //call appendToDom
 appendnewcontactsToDom(data)
-// appendToDom(data)
-
-console.log(data);
 })
 }
 
-button3.addEventListener("click", function(){
-  var gambler_field = $('#gambler-name');
-  var value = $(gambler_field).val().split(" ")
-  // needs to take a hash for json packaging
-  var hash = {name: value[0], amount: value[1]}
-  postEntry(hash);
+button3.addEventListener("submit", function(event){
+  event.preventDefault();
+  var name = $(this).find("input[name='name']").val();
+  var age = $(this).find("input[name='age']").val();
+  var address = $(this).find("input[name='address']").val();
+  var phone = $(this).find("input[name='phone']").val();
+  var picture = $(this).find("input[name='picture']").val();
+  var hash1 = {name: name, age: age, address: address, phone_number: phone, picture: picture}
+  postEntry(hash1);
+  debugger
 });
 
 function appendnewcontactsToDom(data){
-  var appended_info = data.name + " " + data.amount
+  var appended_info = data.name + " " + data.age + " " + data.address + " " + data.phone + " " + data.picture
+  debugger
   $('ul').append('<li>' + appended_info + '</li>')
 //parse data?
 
 }
 
+button4.addEventListener("click", function(){
+var deleteId = $(this).parent().attr('id')
+deletefromDb(deleteId)
+})
+
+
+function deletefromDb(id) {
+
+  $.ajax({
+    url: '/entries/' + id,
+    method: 'DELETE',
+  }).done(function(data)  {
+    console.log(id + "deleted");
+    deleteFromDom(id)
+  }
+  )
+}
+
+function deleteFromDom(id){
+  console.log("delete from dom called")
+  var $liToDelete = $gamblerList.find("#" + id)
+  $liToDelete.remove();
+}
